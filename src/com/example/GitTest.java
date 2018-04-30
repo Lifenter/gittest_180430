9640a875d41a4e84fa4e5cb85ac7cc53e85fbedc
Lifenter-P54 <Lifenter-P54@Lifenter-P54>
2018-04-30 10:29:50 +0900

    충돌처리 - 팀장방안

--- src/com/example/GitTest.java
+++ src/com/example/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
 		System.out.println("안녕 깃허브123");
 		System.out.println("메시지 확인 완료");
 		System.out.println("팀원 작업 충돌 처리 완료");
+		
+		System.out.println("에러코드는 이렇게 처리합시다.");
 	}
 
 }
